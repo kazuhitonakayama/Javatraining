c4333a095372cb0cd273fbb1be7a3872d2479645
kazuhitonakayama <firstjin19990101@gmail.com>
2020-04-18 23:20:23 +0900

    06_done

--- sample.java
+++ sample.java
@@ -3,3 +3,12 @@ class sample {
         System.out.println("こんにちは");
     }
 }
+
+// コンパイラ方式
+// ー処理が早い
+// ーかきなおすたびに手間
+// インタープリタ方式
+// ー書いたらすぐ実行できる
+// ー処理が遅い
+// 思想
+// ーWrite Once, Run Anywhere
